0cd94e7161acba6151b3ecd6c9b156ccdc2e98ad
ibragimov05 <ibragimov.asus15@gmail.com>
2024-05-14 14:23:41 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tic_tac_toe_game/screens/main_page.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(375, 812),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: MainPage(),
+      ),
+    );
+  }
+}
